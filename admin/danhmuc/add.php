e1e4af7d10d8687cd4dc7a72ef5d3a2edfd973ef
ManhQuynh123 <manhtony2001.q@gmail.com>
2023-02-17 21:44:15 +0700

    khoi tao do an va code trang giao dien chinh

---
+++ admin/danhmuc/add.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="row frmtitle">
+        <h1>Thêm Mới Loại Hàng Hóa</h1>
+    </div>
+    <div class="row frmcontent">
+        <form action="#" method="post">
+            <div class="row mb10">
+                MÃ LOẠI<br/>
+                <input type="text" name="maloai" disabled>
+            </div>
+            <div class="row mb10">
+                TÊN LOẠI<br/>
+                <input type="text" name="tenloai">
+            </div>
+            <div class="row mb10">
+                <input type="submit" value="Thêm Mới">
+                <input type="reset" value="Nhập Lại">
+                <a href="#"><input type="button" value="Danh Sách"></a>
+                <input type="checkbox" value="Đặt">
+            </div>
+        </form>
+    </div>
+</div>
