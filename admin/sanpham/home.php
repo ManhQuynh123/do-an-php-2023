5d801350fda6920776e6abba931f7ff26cf1a9ae
Manh Quynh <manhtony2001.q@gmail.com>
2023-04-01 10:14:58 +0700

    Sua lai giao dien xiu

---
+++ admin/sanpham/home.php
@@ -0,0 +1,24 @@
+<div class="row">
+            <div class="row frmtitle">
+                <H1>THÊM MỚI LOẠI HÀNG HOÁ</H1>
+            </div>
+            <div class="row frmcontent">
+                <form action="#" method="post">
+                    <div class="row mb10">
+                        MÃ LOẠI<br>
+                        <input type="text" name="maloai" disabled>
+                    </div>
+                    <div class="row mb10">
+                        TÊN LOẠI<br>
+                        <input type="text" name="tenloai">
+                    </div>
+                    <div class="row mb10">
+                        <input type="submit" name="THÊM MỚI">
+                        <input type="reset" name="NHẬP LẠI">
+                        <a href="danhsach_loaihang.html"><input type="button" value="DANH SÁCH"></a>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
