6c965c942ac57ddf3a4740b60d03eeb1b86c7d51
Manh Quynh <manhtony2001.q@gmail.com>
2023-04-02 10:11:30 +0700

    update Cap Nhat va Sua Danh muc

--- admin/index.php
+++ admin/index.php
@@ -30,8 +30,25 @@
                     $listdanhmuc=pdo_query($sql);
                     include "danhmuc/list.php";
                     break;
-                case 'addsanpham':
-                    include "sanpham/add.php";
+                case 'suasanpham':
+                    if(isset($_GET['id'])&&($_GET['id']>0)){
+                        $sql="select * from danhmuc where id=".$_GET['id'];
+                        $dm=pdo_query_one($sql);
+                    }
+                    
+                    include "sanpham/update.php";
+                    break;
+                case 'updatedanhmuc':
+                    if(isset($_POST['capnhat'])&&($_POST['capnhat'])){
+                        $tenloai=$_POST['tenloai'];
+                        $id=$_POST['id'];
+                        $sql="update danhmuc set name='".$tenloai."' where id=";
+                        pdo_execute($sql);
+                        $thongbao="Cập nhật thành công";
+                    }
+                    $sql="select * from danhmuc order by id desc";
+                    $listdanhmuc=pdo_query_one($sql);
+                    include "sanpham/list.php";
                     break;
             default:
                 include "home.php";
