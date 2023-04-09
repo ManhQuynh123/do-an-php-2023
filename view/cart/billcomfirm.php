e2bf19a1730abc375e914b759a6f5b19b6174586
Hai Nguyen <nnnnguyenphuhaiiii@gmail.com>
2023-04-09 08:28:52 +0700

    add cart view

---
+++ view/cart/billcomfirm.php
@@ -0,0 +1,15 @@
+    <div class="row mb10 header2" >
+        <h1>QUÝ KHÁCH ĐÃ THANH TOÁN THÀNH CÔNGS</h1>
+    </div>
+    <div class="row mb10 header2" >
+        <h1>CẢM ƠN QUÝ KHÁCH ĐÃ TIN VÀO DỊCH VỤ CHÚNG TÔI</h1>
+    </div>
+    <style>
+    .header2 h1{
+        text-align: center;
+    }
+    h1{
+        font-size: 30px;
+        color: darkmagenta;
+    }
+    </style>
